83ca84de06e14547d5377e86813cf99e930f0254
Abhi <abhijithabhi36151@gmail.com>
2025-05-24 15:13:04 +0530

    ui: fix ui things

--- src/app/components/SectionTitle.js
+++ src/app/components/SectionTitle.js
@@ -8,7 +8,7 @@ const SectionTitle = ({title}) => {
   useSectionTitleReveal(sectionTitleRef)
   return (
     <div className="overflow-hidden">
-        <h2 className="section-title text-6xl p-2 sm:p-0 lg:p-0 xl:p-0  md:text-7xl lg:text-9xl xl:text-[10rem]" ref={sectionTitleRef}>{title}</h2>
+        <h2 className="section-title text-5xl p-2 sm:p-0 lg:p-0 xl:p-0  md:text-6xl lg:text-7xl xl:text-[10rem]" ref={sectionTitleRef}>{title}</h2>
       </div>
   )
 }
